48a2ef6ee58d4c6e9742a93feb699eb2e93cd80a
unknown <Amy@.(none)>
2011-07-25 01:10:17 -0500

    Got rid of footer menu.

--- yumeiro/footer.php
+++ yumeiro/footer.php
@@ -1,9 +1,9 @@
             <div id="footer">
                 <p>yumei.ro &copy; <?php echo date('Y'); ?> Yumeko</p>
-                <? wp_nav_menu(array(
+                <? /* wp_nav_menu(array(
                         "menu" => "footer",
                         "menu_class" => "footer-links"
-                )); ?>
+                )); */?>
             </div> <!-- END FOOTER -->
         </div> <!-- END CONTAINER -->
         <?php wp_footer(); ?>
