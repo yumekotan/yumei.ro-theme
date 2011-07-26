ab5a1a39f34525d63740536d0e9141cf82ef04fa
unknown <Amy@.(none)>
2011-07-26 10:29:56 -0500

    Added link and comment images.

--- yumeiro/index.php
+++ yumeiro/index.php
@@ -49,10 +49,10 @@
 					<span class='st_facebook' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' ></span><span class='st_twitter' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' ></span><span class='st_gbuzz' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' ></span><span class='st_email' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' ></span><span class="st_plusone_hcount" st_url="<? the_permalink(); ?>"></span><span class="st_fblike_hcount" st_url="<? the_permalink(); ?>"></span>
 				</div>
 				<div class="post-end">
-					<span class="permalink"><a href="<? the_permalink(); ?>">Permalink</a></span>
+					<span class="permalink"><img class="permalink-image" src="images/permalink.png" /> <a href="<? the_permalink(); ?>">Permalink</a></span>
 					<? if ($is_list) { // just a comment link ?>
                     <span class="comments-link">
-                        <?php comments_popup_link( '<span class="leave-reply">' . __( 'Reply', 'yumeiro' ) . '</span>', _x( '1', 'comments number', 'yumeiro' ), _x( '%', 'comments number', 'yumeiro' ) ); ?>
+                        <img class="comments-image" src="images/comments.png" /> <?php comments_popup_link( '<span class="leave-reply">' . __( 'Reply', 'yumeiro' ) . '</span>', _x( '1', 'comments number', 'yumeiro' ), _x( '%', 'comments number', 'yumeiro' ) ); ?>
                     </span>
 					<? } else { // enabling comments ?>
 						<? comments_template( '', true ); ?>
