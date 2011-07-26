339118a0a56c5baf34bbc2a99b850de4a41a428d
unknown <Amy@.(none)>
2011-07-26 13:56:27 -0500

    Tweaking.

--- yumeiro/index.php
+++ yumeiro/index.php
@@ -46,15 +46,17 @@
                 </p>
                 <? the_content("Read more &raquo;"); ?>
 				<div class="post-end">
-					<span class="permalink"><img class="permalink-image" src="/wp-content/themes/yumeiro/images/permalink.png" /> <a href="<? the_permalink(); ?>">Permalink</a></span>
-					<? if ($is_list) { // just a comment link ?>
-                    <span class="comments-link">
-                        <img class="comments-image" src="/wp-content/themes/yumeiro/images/comments.png" /> <?php comments_popup_link( '<span class="leave-reply">' . __( 'Reply', 'yumeiro' ) . '</span>', _x( '1', 'comments number', 'yumeiro' ), _x( '%', 'comments number', 'yumeiro' ) ); ?>
-                    </span>
-					<? } ?>
-				</div>
-				<div class="sharethis-chicklets">
-					<span class='st_facebook' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' ></span><span class='st_twitter' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' ></span><span class='st_gbuzz' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' ></span><span class='st_email' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' ></span><span class="st_plusone_hcount" st_url="<? the_permalink(); ?>"></span><span class="st_fblike_hcount" st_url="<? the_permalink(); ?>"></span></span>
+					<span class="post-end-links">
+						<span class="permalink"><img class="permalink-image" src="/wp-content/themes/yumeiro/images/permalink.png" /> <a href="<? the_permalink(); ?>">Permalink</a></span>
+						<? if ($is_list) { // just a comment link ?>
+						<span class="comments-link">
+							<img class="comments-image" src="/wp-content/themes/yumeiro/images/comments.png" /> <?php comments_popup_link( '<span class="leave-reply">' . __( 'Reply', 'yumeiro' ) . '</span>', _x( '1', 'comments number', 'yumeiro' ), _x( '%', 'comments number', 'yumeiro' ) ); ?>
+						</span>
+						<? } ?>
+					</span>
+					<span class="sharethis-chicklets">
+						<span class='st_facebook' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' ></span><span class='st_twitter' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' ></span><span class='st_gbuzz' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' ></span><span class='st_email' st_title='<?php the_title(); ?>' st_url='<?php the_permalink(); ?>' ></span><span class="st_plusone_hcount" st_url="<? the_permalink(); ?>"></span><span class="st_fblike_hcount" st_url="<? the_permalink(); ?>"></span></span>
+					</span>
 				</div>
 				<? if (!$is_list) { // enabling comments ?>
 					<div id="comments"><? comments_template( '', true ); ?></div>
