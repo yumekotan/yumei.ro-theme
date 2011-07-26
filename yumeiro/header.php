a7de73562cecf048ed0fd6c0dca360d6f2a136be
unknown <Amy@.(none)>
2011-07-25 20:10:38 -0500

    Trying to fix links.

--- yumeiro/header.php
+++ yumeiro/header.php
@@ -4,8 +4,8 @@
     <head>
         <title>yumei.ro 「夢色」 what color is your dream?</title>
         <link href="<?php bloginfo( 'stylesheet_url' ); ?>" type="text/css" rel="stylesheet"/>
-		<script type="text/javascript">var disqus_iframe_css = "http://yumei.ro/disqus.css";</script>
-		<link href="disqus.css" type="text/css" rel="stylesheet" />
+		<script type="text/javascript">var disqus_iframe_css = "/wp-content/themes/yumeiro/disqus.css";</script>
+		<link href="/wp-content/themes/yumeiro/disqus.css" type="text/css" rel="stylesheet" />
         <?php wp_head(); ?>
     </head>
 
