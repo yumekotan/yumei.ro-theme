437b451ada63b1e5c909a860242f9f4c3b3f9f11
unknown <Amy@.(none)>
2011-07-26 15:30:02 -0500

    Testing AddThis.

--- yumeiro/index.php
+++ yumeiro/index.php
@@ -53,6 +53,13 @@
 							<img id="comments-image" src="/wp-content/themes/yumeiro/images/comments.png" /> <?php comments_popup_link( '<span class="leave-reply">' . __( 'Reply', 'yumeiro' ) . '</span>', _x( '1', 'comments number', 'yumeiro' ), _x( '%', 'comments number', 'yumeiro' ) ); ?>
 						</span>
 						<? } ?>
+						<div class="addthis_toolbox addthis_default_style">
+							<a class="addthis_button_preferred_1"></a>
+							<a class="addthis_button_preferred_2"></a>
+							<a class="addthis_button_preferred_3"></a>
+							<a class="addthis_button_preferred_4"></a>
+							<a class="addthis_button_compact"></a>
+						</div>
 					</span>
 				</div>
 				<? if (!$is_list) { // enabling comments ?>
